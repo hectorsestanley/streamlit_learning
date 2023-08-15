0e135e03b9f00cc48999e988ae1a3a7acd79cf16
hectorsestanley <62483627+hectorsestanley@users.noreply.github.com>
2023-08-15 22:23:04 +0100

    Update new_project_learning.py

--- new_project_learning.py
+++ new_project_learning.py
@@ -1,10 +1,36 @@
+# Import necessary libraries
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.set_page_config(
-  page_title="multipage app",
-  page_icon="🌊 ",
-)
+# Function for the Home Page
+def home_page():
+    st.title("Home Page")
+    st.write("Welcome to the multi-page Streamlit app!")
+    st.write("Navigate to the pages using the sidebar.")
 
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
+# Function for the Data Page
+def data_page():
+    st.title("Data Page")
+    st.write("This is the data page. Here's a sample dataframe:")
+    
+    # Generating a sample dataframe
+    df = pd.DataFrame({
+        'A': np.random.rand(50),
+        'B': np.random.rand(50),
+        'C': np.random.randint(0, 100, 50)
+    })
+    
+    st.write(df)
 
+# Function for the Visualization Page
+def visualization_page():
+    st.title("Visualization Page")
+    st.write("Here's a sample plot:")
+
+    # Generating random data for plotting
+    x = np.linspace(0, 2 * np.pi, 400)
+    y = np.sin(x ** 2)
+    
+   
