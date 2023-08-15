7270ef91fe6a05082cbd2c7bcff69bca13a68731
hectorsestanley <62483627+hectorsestanley@users.noreply.github.com>
2023-08-15 22:20:35 +0100

    Update new_project_learning.py

--- new_project_learning.py
+++ new_project_learning.py
@@ -1,1 +1,10 @@
-print ("hello world")
+import streamlit as st
+
+st.set_page_config(
+  page_title="multipage app",
+  page_icon="🌊 ",
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+
